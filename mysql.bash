3c1371aea05261f441d96b49ce424823de091a3e
Bastien Michaux <bmichaux@altissia.com>
2018-03-05 17:38:18 +0100

    add mysql stop alias

--- mysql.bash
+++ mysql.bash
@@ -6,4 +6,4 @@ alias mysqlConnect='mysql -u root -p';
 alias mysqlRestart='sudo /etc/init.d/mysql restart';
 alias mysqlStart='sudo /etc/init.d/mysql start';
 alias mysqlStatus='sudo service mysql status';
-# alias mysqlStop='dunno yet lol';
+alias mysqlStop='sudo /etc/init.d/mysql stop';
